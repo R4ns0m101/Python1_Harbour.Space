79e0e946f9ae37dc0a5895cc816dd456496a8fbc
R4ns0m101 <nhyueakritphat@gmail.com>
2025-10-21 15:16:17 +0700

    init

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-print("Hello World")
-print("Goodbye")
-print(1,2,6,"Hello")
-
-#single comment
-'''
-mulitple comment
-'''
-#function_name(input1,input2) -> argument mean input of function
-
-#sep optional that insert between the output
-print(1,2,6,"Hello", sep="-")
+print("kkkkkkk")
+print(bool(0))
