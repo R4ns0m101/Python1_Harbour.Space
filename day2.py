8e0ccba997aedb771361822c80f84d3a7627827d
R4ns0m101 <nhyueakritphat@gmail.com>
2025-10-21 15:28:00 +0700

    Lecture Day-2

--- day2.py
+++ day2.py
@@ -13,21 +13,39 @@ print(1,2,6,"Hello", sep="-")
 
 #Data_type
 
-# #number
-# #integer
-# 3,-4,5,20
-# #float
-# 3.5,0.24,-4.2,5.00
-# #string -> text
-# "Hello"
-# "0.00"
-# #boolean
-# True,False
+#number
+#integer
+3,-4,5,20
+#float
+3.5,0.24,-4.2,5.00
+#string -> text
+"Hello"
+"0.00"
+#boolean
+True,False
+print(bool(0)) #False otherwise is True 
+print(bool("")) #False otherwise is True empty 
+
 # #None type
-# None
+None
 
 #check type
 print(type(input))
 
 #check -0 is int or not the result is True or False 
 print(isinstance(-0,int))
+print(isinstance(False,int)) #True because False,True that show at 0,1
+
+
+#convert data type
+# str(4) -> 4 is strings
+
+#Variable 
+#snake case
+founded_yesr_utcc_admission_department = 1980
+
+#camel case
+foundedYearUtccAdmssionDepartment = 1980
+
+#Pascal case
+FoundedYearUtccAdmssionDepartment = 1980
