74f09ecba82ad773ad1cc00dfaaa25119a4fc52a
R4ns0m101 <nhyueakritphat@gmail.com>
2025-10-21 14:40:54 +0700

    lecture Day 2

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("kkkkkkk")
+print("Hello World")
+print("Goodbye")
+print(1,2,6,"Hello")
+
+#single comment
+'''
+mulitple comment
+'''
+#function_name(input1,input2) -> argument mean input of function
+
+#sep optional that insert between the output
+print(1,2,6,"Hello", sep="-")
